a81e9aedf21c077c37c4a135a2c6c9fd89387917
Niall010 <niall010@hotmail.com>
2021-11-10 19:22:15 +0000

    Added frame rate and send data functionality

--- app/src/main/java/com/example/inertiaphoneapp/MainActivity.java
+++ app/src/main/java/com/example/inertiaphoneapp/MainActivity.java
@@ -9,19 +9,28 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.net.Socket;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "MainActivity";
+
+    private int frame;
+    private long frameTime;
+    private float millisPerFrame;
+    long elapsedTimeMillis;
+    private boolean isSendingData;
 
     //Init up sensor manager
     private SensorManager sensorManager;
@@ -37,19 +46,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView linearAccelerationText;
     private TextView rotationVectorText;
     private TextView gyroText;
+    private TextView frameRateText;
     private Button sendDataButton;
+    private Button setFrameRateButton;
 
-    //Vectors
-    float accelerometerVector[];
-    float gravityVector[];
-    float gyroVector[];
-    float linearAccelerationVector[];
-    float rotationVector[];
+    private InertiaData inertiaData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frame = 0;
+        millisPerFrame = 1000/60; //60fps initialised
+        frameTime = System.currentTimeMillis();
+        isSendingData = false;
+        inertiaData = new InertiaData();
 
         //Get UI Elements
         accelerometerText = findViewById(R.id.accelerometerText);
@@ -58,6 +71,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         rotationVectorText = findViewById(R.id.rotationVectorText);
         gyroText = findViewById(R.id.gyroText);
         sendDataButton = findViewById(R.id.sendDataButton);
+        setFrameRateButton = findViewById(R.id.setFrameRateButton);
+        frameRateText = findViewById(R.id.frameRateText);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
@@ -77,42 +92,67 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sendDataButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendData(v);
+              isSendingData = !isSendingData;
             }
         });
 
+        setFrameRateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                millisPerFrame = 1000/Long.parseLong(frameRateText.getText().toString());
+            }
+        });
 
     }
+
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         //Assign UI live data#
         Sensor sensor = event.sensor;
-        String text;
+        String text = "";
+        elapsedTimeMillis = System.currentTimeMillis() - frameTime;
 
+
+        //Get most recent sensor update and assign to class object inertiaData + print
         if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            text = ("X: " + round(event.values[0],2) + " Y: " + round( event.values[1],2) + " Z: " + round(event.values[2],2));
+            text = ("X: " + round(event.values[0], 2) + " Y: " + round(event.values[1], 2) + " Z: " + round(event.values[2], 2));
+            inertiaData.setAccelerometerVector(event.values);
             accelerometerText.setText(text);
         } else if (sensor.getType() == Sensor.TYPE_GRAVITY) {
-            text = ("X: " + round(event.values[0],2) + " Y: " + round( event.values[1],2) + " Z: " + round(event.values[2],2));
+            text = ("X: " + round(event.values[0], 2) + " Y: " + round(event.values[1], 2) + " Z: " + round(event.values[2], 2));
+            inertiaData.setGravityVector(event.values);
             gravityText.setText(text);
         } else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            text = ("X: " + round(event.values[0],2) + " Y: " + round( event.values[1],2) + " Z: " + round(event.values[2],2));
+            text = ("X: " + round(event.values[0], 2) + " Y: " + round(event.values[1], 2) + " Z: " + round(event.values[2], 2));
+            inertiaData.setGyroVector(event.values);
             gyroText.setText(text);
         } else if (sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-            text = ("X: " + round(event.values[0],2) + " Y: " + round( event.values[1],2) + " Z: " + round(event.values[2],2));
+            inertiaData.setLinearAccelerationVector(event.values);
+            text = ("X: " + round(event.values[0], 2) + " Y: " + round(event.values[1], 2) + " Z: " + round(event.values[2], 2));
             linearAccelerationText.setText(text);
         } else if (sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-            text = ("X: " + round(event.values[0],2) + " Y: " + round( event.values[1],2) + " Z: " + round(event.values[2],2) + " Vector: " + round(event.values[3],2));
+            inertiaData.setRotationVector(event.values);
+            text = ("X: " + round(event.values[0], 2) + " Y: " + round(event.values[1], 2) + " Z: " + round(event.values[2], 2) + " Vector: " + round(event.values[3], 2));
             rotationVectorText.setText(text);
         }
 
-    }
-        @Override
-        public void onAccuracyChanged (Sensor sensor,int accuracy){
-
+        if(isSendingData == true) {
+            //send data every 16 milliseconds, therefore 999 every second almost 60fps
+            if (elapsedTimeMillis > frame) {
+                frameTime = System.currentTimeMillis();
+                BackgroundTask b1 = new BackgroundTask();
+                b1.execute("frame " +  frame + " " + inertiaData.convertToString(inertiaData));
+                frame++;
+            }
         }
+    }
+
+    @Override
+    public void onAccuracyChanged (Sensor sensor,int accuracy){
+
+    }
 
     public static float round(float d, int decimalPlace) {
         BigDecimal bd = new BigDecimal(Float.toString(d));
@@ -122,18 +162,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
-
-    public void sendData(View v)
-    {
-        String message = accelerometerText.getText().toString();
-
-        BackgroundTask b1 = new BackgroundTask();
-        b1.execute(message);
-    }
-
     class BackgroundTask extends AsyncTask<String,Void,Void>
     {
-
         Socket s;
         PrintWriter writer;
 
@@ -141,9 +171,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         protected Void doInBackground(String... voids) {
             try {
                 String message = voids[0];
-                s = new Socket(" 192.168.1.78", 6000);
+                s = new Socket("192.168.1.78", 6000);
                 writer = new PrintWriter(s.getOutputStream());
-                writer.write(message);
+                writer.write(String.valueOf(message));
                 writer.flush();
                 writer.close();
                 s.close();
@@ -154,8 +184,49 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
             return null;
         }
+    }
 
+    class InertiaData
+    {
+        //Vectors
+        float accelerometerVector[];
+        float gravityVector[];
+        float gyroVector[];
+        float linearAccelerationVector[];
+        float rotationVector[];
 
+        public void setAccelerometerVector(float[] accelerometerVector) { this.accelerometerVector = accelerometerVector; }
+
+        public void setGravityVector(float[] gravityVector) { this.gravityVector = gravityVector; }
+
+        public void setGyroVector(float[] gyroVector) { this.gyroVector = gyroVector; }
+
+        public void setLinearAccelerationVector(float[] linearAccelerationVector) { this.linearAccelerationVector = linearAccelerationVector; }
+
+        public void setRotationVector(float[] rotationVector) { this.rotationVector = rotationVector; }
+
+        public float[] getAccelerometerVector() { return accelerometerVector; }
+
+        public float[] getGravityVector() { return gravityVector; }
+
+        public float[] getGyroVector() { return gyroVector; }
+
+        public float[] getLinearAccelerationVector() { return linearAccelerationVector; }
+
+        public float[] getRotationVector() { return rotationVector; }
+
+        public String convertToString(InertiaData data)
+        {
+            String text = "";
+            text = ("Accelerometer:" + Arrays.toString(data.getAccelerometerVector()) +
+                    "Gravity:" + Arrays.toString(data.getGravityVector()) +
+                    "Gyro:" + Arrays.toString(data.getGyroVector()) +
+                    "LinearAcceleration:" + Arrays.toString(data.getLinearAccelerationVector()) +
+                    "Rotation:" + Arrays.toString(data.getRotationVector())
+            );
+
+            return text;
+        }
     }
 
 }
